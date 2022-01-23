50cc55383028b76a726ad04c0a251ecba6403277
Signalogic, Inc <info@signalogic.com>
2022-01-23 06:47:18 -0600

    v7 install script

--- rar_packages/autoInstall_SigSRF_SDK_2022v7.sh
+++ rar_packages/autoInstall_SigSRF_SDK_2022v7.sh
@@ -291,7 +291,9 @@ dependencyCheck() {  # Check for generic sw packages and prompt for installation
 			e=$d
 		else
          if [[ "$d" == *"-devel-"* ]]; then
-			   e=$(sed 's/-[^.]*//2' <<< $line)  # search for second "-" to set e with generic developer package name
+   echo
+   echo "inside -devel-"
+			   e=$(sed 's/-//2' <<< $line)  # search for second "-" to set e with generic developer package name
          else
             e=$(sed 's/-.*//g' <<< $line)  # search for first "-" to set e with generic package name
          fi
