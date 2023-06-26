4515de4b49199ac44af2502e578590061d912712
Signalogic, Inc <info@signalogic.com>
2023-06-26 14:31:27 -0500

    update license statement to exclude AI programming models

--- libs/alglib/fs_conv.c
+++ libs/alglib/fs_conv.c
@@ -5,6 +5,8 @@
   
  Copyright (C) Signalogic, 2001-2022
 
+  Use and distribution of this source code is subject to terms and conditions of the Github SigSRF License v1.1, published at https://github.com/signalogic/SigSRF_SDK/blob/master/LICENSE.md. Absolutely prohibited for AI language or programming model training use
+
  Revision History
 
    Created  April 2001 Rastko Selmic
